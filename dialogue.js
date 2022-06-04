fec974f9466b5e400b09bc73b2c69d3fb669e5a2
ihyeon908 <confrontino42@gmail.com>
2022-06-04 23:48:27 +0900

    add basic html css js file

---
+++ dialogue.js
@@ -0,0 +1,131 @@
+
+
+//대사모음
+
+//아무것도 없을 때
+let nothingDlg = [
+  "input number doesn't exist.",
+  'Not Acceptable',
+  'Method not allowed',
+  "Can't find the object",
+  'An error occurred while creating an error report.',
+  "Can't find a button",
+  'Congratulations.', 
+  'Time remaining: About 20304958393 hours.',
+  "Run as fast as you can and don't look back.",
+  'User error - Replace User',
+  'Task failed successfully.',
+];
+let nothingDlgKor = [
+  '입력값이 존재하지 않습니다',
+  '허용되지 않는 요청입니다',
+  '허용되지 않는 메소드',
+  '오브젝트를 찾을 수 없습니다',
+  '에러 메세지로 인해 에러가 발생하였습니다',
+  '버튼을 찾을 수 없습니다',
+  '축하합니다.',
+  '남은 시간: 20304958393 시간.',
+  '뒤돌아보지 말고 최대한 빨리 달리세요.',
+  '사용자 에러 -  교체된 사용자',
+  '성공적으로 실패하였습니다.',
+];
+
+//10번 이상
+let nothingDipDlg = [
+  'Did you sleep well recently?',
+  'You do that again and see what happens…',
+];
+let nothingDipDlgKor = [
+  '최근에 잘 주무셨나요?',
+  '또 그런짓을 하면 어떻게 될지 두고봅시다.',
+];
+
+//맞았을 때
+let correctDlg = [
+  'input number match perfectly.',
+  'Accepted',
+  'Unauthorized',
+  'Request timeout',
+  'Internal Server Error',
+  'Unkown error',
+  'You have entered an illegal object name character',
+  'You must stop what you are doing and acknowledge me now',
+  'Go outside and see the sun for once and enjoy life.',
+  'You will receive an error message in the future.',
+  'Your device is lonely.',
+  "Error code 42: User Error. It's not our fault!",
+  'Cannot save X for reason Y.',
+];
+let correctDlgKor = [
+  '입력값이 완벽히 일치합니다',
+  '전송 완료',
+  '권한없음',
+  '요청시간이 지났습니다',
+  '서버 내부 오류',
+  '알 수 없는 에러',
+  '불법적인 오브젝트명을 입력하였습니다',
+  '하던 것을 멈추고 저를 인지하세요',
+  '밖에 나가서 햇볕도 보고 삶을 즐기세요.',
+  '향후에 에러 메세지를 받게 될겁니다.',
+  '당신의 기기가 외로워합니다.',
+  '에러코드 42: 사용자 에러. 저희의 잘못이 아닙니다!',
+  'X로 인해 Y를 구할 수 없습니다.',
+];
+
+//10번 이상
+let correctDipDlg = [
+  'Did you sleep well recently?',
+  'You do that again and see what happens…',
+];
+let correctDipDlgKor = [
+  '최근에 잘 주무셨나요?',
+  '또 그런짓을 하면 어떻게 될지 두고봅시다.',
+];
+
+//다를때
+let differentDlg = [
+  "input number doesn't match",
+  'Bad Request',
+  'Forbidden',
+  'Service Unavailable',
+  'Catasrophic failure!', 
+  'The operation completed successfully.',
+  "You have gotten so many error messages recently, so here is a random one to let you know that I don't care.",
+  "Found infected file: 'Eight.exe'. You can't remove it.",
+  'You just moved your mouse. Please restart your device.',
+  'If you want to do something useful, get a proper computer, and a mouse more than one button. Ask an adult to help you buy one.',
+  'Are you drunk?',
+  'Please leave a dislike.',
+  "I'm worried about you.",
+];
+let differentDlgKor = [
+  '입력값이 일치하지 않습니다',
+  '요청 실패',
+  '금지',
+  '서비스 불가능 상태입니다',
+  '치명적인 실패!',
+  '완벽히 작동되고 있습니다.',
+  '지나치게 많은 에러메세지를 전송받고 있으므로, 제가 신경쓰지 않는다는 걸 알리고자 무작위 에러를 내보냅니다.',
+  '감염된 파일을 발견했습니다: "Eight.exe". 삭제할 수 없습니다.',
+  '마우스를 움직였으므로 기기를 재시작하세요.',
+  '유용한 일을 하고 싶다면, 좋은 컴퓨터와 하나이상의 버튼이 있는 마우스를 장만하세요. 어른에게 구매를 부탁하세요.',
+  '술에 취했나요?',
+  '싫어요를 남겨주세요.',
+  '당신이 걱정됩니다.',
+];
+
+//10번 이상
+let differentDipDlg = [
+  'Did you sleep well recently?',
+  'You do that again and see what happens…',
+];
+let differentDipDlgKor = [
+  '최근에 잘 주무셨나요?',
+  '또 그런짓을 하면 어떻게 될지 두고봅시다.',
+];
+
+
+let eventDlg = {
+  love : ['event', '이벤트'],
+  stanley : ['event2', '이벤트2']
+};
---
+++ script.js
@@ -0,0 +1,421 @@
+
+const orderbtn = ['Escape', 'Delete', 'Shift', 'Control', 'ArrowLeft', 'ArrowUp', 'ArrowRight', 'ArrowDown', 'F2', 'F6', 'F8', 'F9', '8', 's', 'y', 'u', 'w', 'p', 'm', 'v', 'n', 'z', 'o', 'q'];
+let memory = ['dummy', 'Escape'];
+let ordertime = 0;
+let keynum = 0;
+let i = 0;
+
+//대사 횟수
+let nonum = 0;
+let samenum = 0;
+let diffnum = 0; //지정을 안했으니 당연히 동작을 안하지..
+let eventnum = 0;
+
+//언어 여부 0이 영어 1이 한글
+let lang = 0;
+
+//대사 번호
+let noDlgnum = 0;
+let sameDlgnum = 0;
+let diffDlgnum = 0;
+
+let noDipDlgnum = 0;
+let sameDipDlgnum = 0;
+let diffDipDlgnum = 0;
+
+//첫 시작 특수한 키
+
+document.getElementsByClassName('input')[i].addEventListener('keydown', function(event) {
+  const key = event.key; 
+
+  if (key === 'Delete' || key === 'Escape' || key === 'CapsLock' || key === 'Shift' || key === 'Control' || key === 'ArrowLeft' || key === 'ArrowUp' || key === 'ArrowRight' || key === 'ArrowDown' || key === 'F2' || key === 'F6' || key === 'F8' || key === 'F9') {     
+    document.getElementsByClassName('input')[i].value = key;
+  }
+});
+
+function addLoader() {
+  if (event.keyCode==13) {
+    i++;
+    //console.log('i: ', i);
+
+    //무작위 변수
+    keynum = Math.floor(Math.random()*orderbtn.length);
+
+    //박스 추가
+    var _div = document.createElement('div');
+    _div.setAttribute('class', 'orderbox');
+ 
+    //한국어라면
+    if (lang === 1) {
+
+      _div.innerHTML = `
+      <div class="divisionline">---</div> 
+        <div class="order"> 
+          <div class="textstart">></div>` + 
+  
+          //명령어 언어 바꾸기
+          `<div class="ordercontent" style="display: none;">Please PRESS '${orderbtn[keynum]}' on your KEYBOARD for ${Math.floor((Math.random() * (70 - 10) + 10))}ms.</div> 
+          <div class="ordercontentkor" style="display: inline-block;">키보드에 있는 '${orderbtn[keynum]}' 버튼을 ${Math.floor((Math.random() * (70 - 10) + 10))}ms동안 누르십시오.</div>`
+  
+        + `</div> 
+      <input type="text" class="input" onKeyPress="addLoader()" spellcheck="false" style="font-family: NeoDunggeunmo">
+      `; //인풋에 키보드 누를 때 함수 실행해주는 속성 부여해야 새로 생긴 인풋 텍스트박스 동작함
+
+      //document.getElementsByClassName("input")[i-1].style.fontFamily = 'NeoDunggeunmo';
+    }
+
+    //영어라면
+    if (lang === 0) {
+
+      _div.innerHTML = `
+      <div class="divisionline">---</div> 
+        <div class="order"> 
+          <div class="textstart">></div>` + 
+  
+          //명령어 언어 바꾸기
+          `<div class="ordercontent" style="display: inline-block;">Please PRESS '${orderbtn[keynum]}' on your KEYBOARD for ${Math.floor((Math.random() * (70 - 10) + 10))}ms.</div> 
+          <div class="ordercontentkor" style="display: none;">키보드에 있는 '${orderbtn[keynum]}' 버튼을 ${Math.floor((Math.random() * (70 - 10) + 10))}ms동안 누르십시오.</div>`
+  
+        + `</div> 
+      <input type="text" class="input" onKeyPress="addLoader()" spellcheck="false" style="font-family: Roboto Mono">
+      `; //인풋에 키보드 누를 때 함수 실행해주는 속성 부여해야 새로 생긴 인풋 텍스트박스 동작함
+
+      //document.getElementsByClassName("input")[i].style.fontFamily = 'Roboto Mono';
+    }
+
+    //append _div to keypress
+    document.getElementById("Instruction").appendChild(_div);
+
+    //포커스 위치
+    document.getElementsByClassName('input')[i].focus();
+
+    //전에 썼던 텍스트 박스는 편집 불가능하게
+    document.getElementsByClassName('input')[i-1].readOnly = true;
+
+    //특수한 키 적용하기
+    document.getElementsByClassName('input')[i].addEventListener('keydown', function(event) {
+      const key = event.key; 
+
+      if (key === 'Delete' || key === 'Escape' || key === 'CapsLock' || key === 'Shift' || key === 'Control' || key === 'ArrowLeft' || key === 'ArrowUp' || key === 'ArrowRight' || key === 'ArrowDown' || key === 'F2' || key === 'F6' || key === 'F8' || key === 'F9') {     
+        document.getElementsByClassName('input')[i].value = key;
+      }
+    });
+
+    //전에 내렸던 지시 기억하기
+    memory.shift();
+    memory.push(orderbtn[keynum]);
+
+    //다른내용 입력했을 때 생성되는 p 찾기 위해 번호 부여 //이걸 여기다 놓으면 엔터 칠때마다 0이 되잖아..
+    //let diffnum = 0;
+    //각각에 변수 부여해줬어야 했음
+
+    //만약 내용 없으면
+    if (document.getElementsByClassName('input')[i-1].value.length === 0) {
+      nonum++;
+      noDlgnum = Math.floor(Math.random()*nothingDlg.length);
+      noDipDlgnum = Math.floor(Math.random()*nothingDipDlg.length);
+
+        //전체 상자 생성
+        var emptyCreator = document.createElement('div');
+        emptyCreator.setAttribute('class', 'empty');
+  
+        //내용입력
+        emptyCreator.innerHTML = `
+        <p class="emptyeng">--${nothingDlg[noDlgnum]}</p> 
+        <p class="emptykor">--${nothingDlgKor[noDlgnum]}</p>`;
+
+        //만약 깊게 들어가면 //리스트 추가하기?
+        if (nonum >= 10) {
+          emptyCreator.innerHTML = `
+          <p class="emptyeng">--${nothingDipDlg[noDipDlgnum]}</p> 
+          <p class="emptykor">--${nothingDipDlgKor[noDipDlgnum]}</p>`;
+        }
+
+        //적용하기
+        document.getElementsByClassName('orderbox')[i-1].appendChild(emptyCreator);
+  
+        //언어여부
+        if (lang === 1) {
+          let children = document.getElementsByClassName("empty")[nonum-1].getElementsByTagName("*");
+          for (let item of children) {
+            item.style.fontFamily = 'NeoDunggeunmo';
+          }
+          
+          document.getElementsByClassName("emptykor")[nonum-1].style.display = 'inline-block';
+          document.getElementsByClassName("emptyeng")[nonum-1].style.display = 'none';
+        }
+  
+        if (lang === 0) {
+  
+          let children = document.getElementsByClassName("empty")[nonum-1].getElementsByTagName("*");
+          for (let item of children) {
+            item.style.fontFamily = 'Roboto Mono;';
+          }
+          document.getElementsByClassName("emptyeng")[nonum-1].style.display = 'inline-block';
+          document.getElementsByClassName("emptykor")[nonum-1].style.display = 'none';
+        }
+    }
+
+    //만약 내용 같으면 //전에 썼던 코드 기억해서 가져와야함
+    if (document.getElementsByClassName('input')[i-1].value === memory[0] ) {
+      samenum++;
+      sameDlgnum = Math.floor(Math.random()*correctDlg.length);
+      sameDipDlgnum = Math.floor(Math.random()*correctDipDlg.length);
+
+      //전체 상자 생성
+      var sameCreator = document.createElement('div');
+      sameCreator.setAttribute('class', 'same');
+
+      //내용입력
+      sameCreator.innerHTML = `
+      <p class="sameeng">--${correctDlg[sameDlgnum]}</p> 
+      <p class="samekor">--${correctDlgKor[sameDlgnum]}</p>`;
+
+      //깊게 들어가면
+      if (samenum >= 10) {
+        sameCreator.innerHTML = `
+        <p class="sameeng">--${correctDipDlg[sameDipDlgnum]}</p> 
+        <p class="samekor">--${correctDipDlgKor[sameDipDlgnum]}</p>`;
+      }
+
+      //적용하기
+      document.getElementsByClassName('orderbox')[i-1].appendChild(sameCreator);
+
+      //언어여부
+      if (lang === 1) {
+        let children = document.getElementsByClassName("same")[samenum-1].getElementsByTagName("*");
+        for (let item of children) {
+          item.style.fontFamily = 'NeoDunggeunmo';
+        }
+        
+        document.getElementsByClassName("samekor")[samenum-1].style.display = 'inline-block';
+        document.getElementsByClassName("sameeng")[samenum-1].style.display = 'none';
+      }
+
+      if (lang === 0) {
+
+        let children = document.getElementsByClassName("same")[samenum-1].getElementsByTagName("*");
+        for (let item of children) {
+          item.style.fontFamily = 'Roboto Mono;';
+        }
+        document.getElementsByClassName("sameeng")[samenum-1].style.display = 'inline-block';
+        document.getElementsByClassName("samekor")[samenum-1].style.display = 'none';
+      }
+    }
+
+    //만약 내용 다르면 //내용 다른데 일단 입력하기는 했을 때
+    if (document.getElementsByClassName('input')[i-1].value != memory[0] && document.getElementsByClassName('input')[i-1].value.length != 0) {
+      diffnum++;
+      diffDlgnum = Math.floor(Math.random()*differentDlg.length);
+      diffDipDlgnum = Math.floor(Math.random()*differentDipDlg.length);
+
+      //전체 상자 생성
+      var diffCreator = document.createElement('div');
+      diffCreator.setAttribute('class', 'diff');
+
+      //내용입력
+      diffCreator.innerHTML = `
+      <p class="diffeng">--${differentDlg[diffDlgnum]}</p> 
+      <p class="diffkor">--${differentDlgKor[diffDlgnum]}</p> `;
+
+      //깊게 들어가면
+      if (diffnum >= 10) {
+        diffCreator.innerHTML = `
+        <p class="diffeng">--${differentDipDlg[diffDipDlgnum]}</p> 
+        <p class="diffkor">--${differentDipDlgKor[diffDipDlgnum]}</p> `;
+      }
+
+      //적용하기
+      document.getElementsByClassName('orderbox')[i-1].appendChild(diffCreator);
+
+      //언어 여부
+      if (lang === 1) {
+        let children = document.getElementsByClassName("diff")[diffnum-1].getElementsByTagName("*");
+        for (let item of children) {
+          item.style.fontFamily = 'NeoDunggeunmo';
+        }
+        
+        document.getElementsByClassName("diffkor")[diffnum-1].style.display = 'inline-block';
+        document.getElementsByClassName("diffeng")[diffnum-1].style.display = 'none';
+        
+      }
+      if (lang === 0) {
+
+        let children = document.getElementsByClassName("diff")[diffnum-1].getElementsByTagName("*");
+        for (let item of children) {
+          item.style.fontFamily = 'Roboto Mono;';
+        }
+        document.getElementsByClassName("diffeng")[diffnum-1].style.display = 'inline-block';
+        document.getElementsByClassName("diffkor")[diffnum-1].style.display = 'none';
+      }
+    }
+
+    //이벤트
+    if (eventDlg.hasOwnProperty(document.getElementsByClassName('input')[i-1].value)) {
+      eventnum++;
+      var eventinput = document.getElementsByClassName('input')[i-1];
+
+      //전체 상자 생성
+      var eventCreator = document.createElement('div');
+      eventCreator.setAttribute('class', 'event');
+
+      //내용입력
+      eventCreator.innerHTML = `
+      <p class="eventeng">--${eventDlg[`${eventinput.value}`][0]}</p> 
+      <p class="eventkor">--${eventDlg[`${eventinput.value}`][1]}</p> `;
+
+      //적용하기
+      document.getElementsByClassName('orderbox')[i-1].appendChild(eventCreator);
+
+      //언어 여부
+      if (lang === 1) {
+        let children = document.getElementsByClassName("event")[eventnum-1].getElementsByTagName("*");
+        for (let item of children) {
+          item.style.fontFamily = 'NeoDunggeunmo';
+        }
+        
+        document.getElementsByClassName("eventkor")[eventnum-1].style.display = 'inline-block';
+        document.getElementsByClassName("eventeng")[eventnum-1].style.display = 'none';
+        
+      }
+      if (lang === 0) {
+
+        let children = document.getElementsByClassName("event")[eventnum-1].getElementsByTagName("*");
+        for (let item of children) {
+          item.style.fontFamily = 'Roboto Mono;';
+        }
+        document.getElementsByClassName("eventeng")[eventnum-1].style.display = 'inline-block';
+        document.getElementsByClassName("eventkor")[eventnum-1].style.display = 'none';
+      }
+
+      //내용 다를 때 나타나는 문구 숨기기
+      document.getElementsByClassName('diff')[diffnum-1].style.display = 'none';
+    }
+  }
+}
+
+function translateLang() { //함수명 너무 흔한걸로 쓰면 원래 있던 메소드와 겹쳐버림
+
+  //영어일때
+  if (lang === 1) {
+
+    //버튼 바꾸기
+    document.querySelector('button').innerHTML = '번역';
+    document.querySelector('button').style.fontFamily = 'NeoDunggeunmo';
+
+    //머리말 바꾸기
+    document.title = 'Employee #427 Instructional Prompt';
+    document.querySelector('h1').innerHTML = 'Employee #427 Instructional Prompt';
+    document.querySelector('h1').style.fontFamily = 'Roboto Mono';
+    document.querySelector('p').style.fontFamily = 'Roboto Mono'; //querySelectorAll은 태그 변형되면 사용될 수 없음
+    document.querySelector('input').style.fontFamily = 'Roboto Mono';
+
+    //명령 바꾸기
+    //기본
+    for (j = 0; j < i+1; j++) {
+      document.getElementsByClassName("ordercontent")[j].style.display = "inline-block";
+      document.getElementsByClassName("ordercontentkor")[j].style.display = "none";
+      document.getElementsByClassName("input")[j].style.fontFamily = 'Roboto Mono';
+    }
+    //입력값 없을 때
+    for (n = 0; n < nonum; n++) {
+      let children = document.getElementsByClassName("empty")[n].getElementsByTagName("*");
+      for (let item of children) {
+        item.style.fontFamily = 'Roboto Mono';
+      }
+      document.getElementsByClassName("emptyeng")[n].style.display = "inline-block";
+      document.getElementsByClassName("emptykor")[n].style.display = "none";
+    }
+    //입력값 같을 때
+    for (s = 0; s < samenum; s++) {
+      let children = document.getElementsByClassName("same")[s].getElementsByTagName("*");
+      for (let item of children) {
+        item.style.fontFamily = 'Roboto Mono';
+      }
+      document.getElementsByClassName("sameeng")[s].style.display = "inline-block";
+      document.getElementsByClassName("samekor")[s].style.display = "none";
+    }
+    //입력값 다를 때
+    for (d = 0; d < diffnum; d++) {
+      let children = document.getElementsByClassName("diff")[d].getElementsByTagName("*");
+      for (let item of children) {
+        item.style.fontFamily = 'Roboto Mono';
+      }
+      document.getElementsByClassName("diffeng")[d].style.display = "inline-block";
+      document.getElementsByClassName("diffkor")[d].style.display = "none";
+    }
+    //이벤트
+    for (e = 0; e < eventnum; e++) {
+      let children = document.getElementsByClassName("event")[e].getElementsByTagName("*");
+      for (let item of children) {
+        item.style.fontFamily = 'Roboto Mono';
+      }
+      document.getElementsByClassName("eventeng")[e].style.display = "inline-block";
+      document.getElementsByClassName("eventkor")[e].style.display = "none";
+    }
+
+    return lang = 0;
+  }
+  //한국어일때
+   if (lang === 0) {
+
+    //버튼 바꾸기
+    document.querySelector('button').innerHTML = 'translate';
+    document.querySelector('button').style.fontFamily = 'Roboto Mono';
+
+    //머리말 바꾸기
+    document.title = '직원 #427 지시사항';
+    document.querySelector('h1').innerHTML = '직원 #427 지시사항';
+    document.querySelector('h1').style.fontFamily = "NeoDunggeunmo";
+    document.querySelector('p').style.fontFamily = "NeoDunggeunmo";
+
+    //명령 바꾸기
+    //기본
+    for (j = 0; j < i+1; j++) {
+      document.getElementsByClassName("ordercontent")[j].style.display = "none";
+      document.getElementsByClassName("ordercontentkor")[j].style.display = "inline-block";
+      document.getElementsByClassName("input")[j].style.fontFamily = 'NeoDunggeunmo';
+    }
+    //입력값 없을 때
+    for (n = 0; n < nonum; n++) {
+      let children = document.getElementsByClassName("empty")[n].getElementsByTagName("*");
+      for (let item of children) {
+        item.style.fontFamily = 'NeoDunggeunmo';
+      }
+      document.getElementsByClassName("emptyeng")[n].style.display = "none";
+      document.getElementsByClassName("emptykor")[n].style.display = "inline-block";
+    }
+    //입력값 같을 때
+    for (s = 0; s < samenum; s++) {
+      let children = document.getElementsByClassName("same")[s].getElementsByTagName("*");
+      for (let item of children) {
+        item.style.fontFamily = 'NeoDunggeunmo';
+      }
+      document.getElementsByClassName("sameeng")[s].style.display = "none";
+      document.getElementsByClassName("samekor")[s].style.display = "inline-block";
+    }
+    //입력값 다를 때
+    for (d = 0; d < diffnum; d++) {
+      let children = document.getElementsByClassName("diff")[d].getElementsByTagName("*");
+      for (let item of children) {
+        item.style.fontFamily = 'NeoDunggeunmo';
+      }
+      document.getElementsByClassName("diffeng")[d].style.display = "none";
+      document.getElementsByClassName("diffkor")[d].style.display = "inline-block";
+    }
+    //이벤트
+    for (e = 0; e < eventnum; e++) {
+      let children = document.getElementsByClassName("event")[e].getElementsByTagName("*");
+      for (let item of children) {
+        item.style.fontFamily = 'NeoDunggeunmo';
+      }
+      document.getElementsByClassName("eventeng")[e].style.display = "none";
+      document.getElementsByClassName("eventkor")[e].style.display = "inline-block";
+    }
+
+    return lang = 1;
+  }
+
+
+}
+
